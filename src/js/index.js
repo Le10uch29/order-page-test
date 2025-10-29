09f59f6369b9fc93fd7d310ee0b8e63dffe103aa
Elnur <kal-el@list.ru>
2025-10-29 12:27:59 +0400

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,4 @@
-(() => {
-  const headerContainer = document.querySelector(".header__container");
-  const navList = document.querySelector(".nav__list");
-  if (headerContainer && navList) {
-
-    let burger = document.querySelector(".nav__burger");
-    if (!burger) {
-      burger = document.createElement("button");
-      burger.classList.add("nav__burger");
-      burger.setAttribute("aria-label", "Open Menu");
-      for (let i = 0; i < 3; i++) {
-        const line = document.createElement("span");
-        burger.appendChild(line);
-      }
-      headerContainer.appendChild(burger);
-    }
-
-    burger.addEventListener("click", () => {
-      burger.classList.toggle("active");
-      navList.classList.toggle("active");
-    });
-  }
-
-  const range = document.getElementById("range");
-  const rangeVal = document.querySelector(".order__range-value");
-  if (range && rangeVal) {
-    const paint = () => {
-      rangeVal.textContent = `${range.value}%`;
-      range.style.background = `linear-gradient(90deg, #42A9ED ${range.value}%, #FFFFFFB2 ${range.value}%)`;
-    };
-    range.addEventListener("input", paint);
-    paint(); 
-  }
-})();
+(()=>{const headerContainer=document.querySelector(".header__container");const navList=document.querySelector(".nav__list");if(headerContainer&&navList){let burger=document.querySelector(".nav__burger");if(!burger){burger=document.createElement("button");burger.classList.add("nav__burger");burger.setAttribute("aria-label","Open Menu");for(let i=0;i<3;i++){const line=document.createElement("span");burger.appendChild(line)}
+headerContainer.appendChild(burger)}
+burger.addEventListener("click",()=>{burger.classList.toggle("active");navList.classList.toggle("active")})}
+const range=document.getElementById("range");const rangeVal=document.querySelector(".order__range-value");if(range&&rangeVal){const paint=()=>{rangeVal.textContent=`${range.value}%`;range.style.background=`linear-gradient(90deg, #42A9ED ${range.value}%, #FFFFFFB2 ${range.value}%)`};range.addEventListener("input",paint);paint()}})()
